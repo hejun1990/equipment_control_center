0b86e5c6ccede4a93e70039eeb12e3ed3b1db2a7
hejun1990 <jun_hejudy@sina.cn>
2020-06-06 18:23:56 +0800

    添加jasypt

--- equipment_management/src/test/java/com/geekbang/equipment/management/EquipmentManagementApplicationTests.java
+++ equipment_management/src/test/java/com/geekbang/equipment/management/EquipmentManagementApplicationTests.java
@@ -1,13 +1,30 @@
 package com.geekbang.equipment.management;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {EquipmentManagementApplication.class})
 class EquipmentManagementApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private StringEncryptor encryptor;
 
+    @Test
+    public void getPass() {
+        String url = encryptor.encrypt("jdbc:mysql://127.0.0.1:3306/equipment_control_center?useUnicode=true&characterEncoding=utf8&serverTimezone=GMT%2B8&useSSL=false");
+        String name = encryptor.encrypt("你的数据库用户名");
+        String password = encryptor.encrypt("你的数据库密码");
+        System.out.println("url: " + url);
+        System.out.println("name: " + name);
+        System.out.println("password: " + password);
+        Assert.assertTrue(url.length() > 0);
+        Assert.assertTrue(name.length() > 0);
+        Assert.assertTrue(password.length() > 0);
+    }
 }
